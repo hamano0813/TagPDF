93f6d94a353e6de2d50b07d947ec414e8402c9dd
Hamano0813 <hamano0813@gmail.com>
2023-12-06 23:05:40 +0800

    # 添加数据库model定义

---
+++ src/core/model.py
@@ -0,0 +1,45 @@
+from sqlalchemy import Integer, String, ForeignKeyConstraint, UniqueConstraint
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+
+
+class Base(DeclarativeBase):  # 基类
+    pass
+
+
+class PDF(Base):  # PDF表
+    __tablename__ = 'pdf'
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True, comment='ID')
+    fp: Mapped[str] = mapped_column(String(250), nullable=True, unique=True, comment='file path')
+    title: Mapped[str] = mapped_column(String(250), nullable=True, comment='title')
+    number: Mapped[str] = mapped_column(String(250), nullable=True, comment='number')
+    publisher: Mapped[str] = mapped_column(String(250), nullable=True, comment='publisher')
+    release: Mapped[int] = mapped_column(Integer, nullable=True, comment='release')
+
+    tags: Mapped[list["TAG"]] = relationship("TAG", secondary="pdf_tag", back_populates="pdf", uselist=True)
+
+    def __repr__(self):
+        return (f"<PDF(fp='{self.fp}', title='{self.title}', number='{self.number}',"
+                f" publisher='{self.publisher}', release='{self.release}')>"
+                f" tag='{self.tags}'>")
+
+class TAG(Base):  # TAG表
+    __tablename__ = 'tag'
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True, comment='ID')
+    tag: Mapped[str] = mapped_column(String(250), nullable=False, unique=True, comment='tag')
+
+    pdf: Mapped[list["PDF"]] = relationship("PDF", secondary="pdf_tag", back_populates="tags", uselist=True)
+
+    def __repr__(self):
+        return f"<TAG({self.tag})>"
+
+
+class PDF_TAG(Base):  # PDF_TAG表
+    __tablename__ = 'pdf_tag'
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True, comment='ID')
+    pdf_id: Mapped[int] = mapped_column(Integer, nullable=False, comment='pdf ID')
+    tag_id: Mapped[int] = mapped_column(Integer, nullable=False, comment='tag ID')
+    __table_args__ = (
+        ForeignKeyConstraint(['pdf_id'], ['pdf.id'], ondelete='CASCADE'),
+        ForeignKeyConstraint(['tag_id'], ['tag.id'], ondelete='CASCADE'),
+        UniqueConstraint('pdf_id', 'tag_id', name='pdf_tag_pdf_id_tag_id_uc'),
+    )
