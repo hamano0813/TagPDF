499b2eeca244d61ec3552b09a03ec5c1aaefc337
HamanoTaiki <hamano0813@gmail.com>
2023-12-11 16:32:26 +0800

    # 重构PDF信息写入框架

--- src/ui/info_frame/info_frame.py
+++ src/ui/info_frame/info_frame.py
@@ -1,7 +1,7 @@
 from PySide6 import QtWidgets, QtCore
 
-from .tag_edit import TagEdit
-from .year_spin import YearSpin
+from ui.info_frame.tag_edit import TagEdit
+from ui.info_frame.year_spin import YearSpin
 from core import functions
 
 
@@ -11,87 +11,88 @@ class InfoFrame(QtWidgets.QFrame):
     def __init__(self, session_maker=None):
         super().__init__(parent=None)
         self.setContentsMargins(0, 0, 0, 0)
-        self.session = session_maker()
-        self.path = ''
+        self._session = session_maker()
+        self._path = ''
 
-        self.tags = TagEdit()
-        self.tags.setProperty("field", "tags")
-        self.tit = self._create_line('tit')
-        self.num = self._create_line('num')
-        self.pub = self._create_line('pub')
-        self.rls = YearSpin()
-        self.rls.setProperty("field", "rls")
-        self.rls.setFixedHeight(self.tags.sizeHint().height())
+        self._tags = TagEdit()
+        self._tit = self._create_line('tit')
+        self._num = self._create_line('num')
+        self._pub = self._create_line('pub')
+        self._rls = YearSpin()
+
+        self._tags.setProperty("field", "tags")
+        self._rls.setProperty("field", "rls")
+        self._rls.setFixedHeight(self._tags.sizeHint().height())
 
         self.setLayout(QtWidgets.QFormLayout())
-        self.layout().addRow("标题", self.tit)
-        self.layout().addRow("文号", self.num)
-        self.layout().addRow("发布", self.pub)
-        self.layout().addRow("年份", self.rls)
-        self.layout().addRow("标签", self.tags)
+        self.layout().addRow("标题", self._tit)
+        self.layout().addRow("文号", self._num)
+        self.layout().addRow("发布", self._pub)
+        self.layout().addRow("年份", self._rls)
+        self.layout().addRow("标签", self._tags)
 
-        self.rls.valueChanged.connect(self.change_info)
-        self.tags.tagChanged.connect(self.change_info)
+        self._rls.valueChanged.connect(self._change_info)
+        self._tags.tagChanged.connect(self._change_info)
 
-        self.refresh_completer()
+        self._reset_completer()
 
     def _create_line(self, field: str):
         line = QtWidgets.QLineEdit()
         line.setObjectName('LineEdit')
         line.setProperty("field", field)
         line.setContentsMargins(0, 0, 0, 0)
-        line.setFixedHeight(self.tags.sizeHint().height())
-        line.editingFinished.connect(self.change_info)
+        line.setFixedHeight(self._tags.sizeHint().height())
+        line.editingFinished.connect(self._change_info)
         line.data = lambda: line.text()
         return line
 
-    def get_info(self):
+    def _get_info(self):
         info = {
-            "tit": self.tit.text() if self.tit.text().strip() else None,
-            "num": self.num.text() if self.tit.text().strip() else None,
-            "pub": self.pub.text() if self.tit.text().strip() else None,
-            "rls": self.rls.value(),
-            "tags": [functions.get_tag_by_tag(self.session, t) for t in self.tags.tags]
+            "tit": self._tit.text() if self._tit.text().strip() else None,
+            "num": self._num.text() if self._tit.text().strip() else None,
+            "pub": self._pub.text() if self._tit.text().strip() else None,
+            "rls": self._rls.value(),
+            "tags": [functions.get_tag_by_tag(self._session, t) for t in self._tags.tags]
         }
         return info
 
-    def set_info(self, info: dict):
-        self.tit.setText(info.get("tit", ""))
-        self.num.setText(info.get("num", ""))
-        self.pub.setText(info.get("pub", ""))
-        self.rls.setValue(info.get("rls", None))
-        self.tags.tags = [t.tag for t in info.get("tags", list())]
+    def _set_info(self, info: dict):
+        self._tit.setText(info.get("tit", ""))
+        self._num.setText(info.get("num", ""))
+        self._pub.setText(info.get("pub", ""))
+        self._rls.setValue(info.get("rls", None))
+        self._tags.tags = [t.tag for t in info.get("tags", list())]
 
-    def change_info(self) -> None:
-        if not self.path:
+    def _change_info(self) -> None:
+        if not self._path:
             return self.sender().clear()
-        if not (pdf := functions.get_pdf_by_path(self.session, self.path)):
-            pdf = functions.create_pdf_by_path(self.session, self.path)
+        if not (pdf := functions.get_pdf_by_path(self._session, self._path)):
+            pdf = functions.create_pdf_by_path(self._session, self._path)
         field = self.sender().property("field")
-        data = self.get_info().get(field)
-        if self.get_info().get("tit"):
-            functions.update_pdf_with_field(self.session, pdf, field, data)
+        data = self._get_info().get(field)
+        if self._get_info().get("tit"):
+            functions.update_pdf_with_field(self._session, pdf, field, data)
         else:
-            functions.delete_pdf(self.session, pdf)
+            functions.delete_pdf(self._session, pdf)
             self.clear()
         self.infoChanged.emit()
-        self.refresh_completer()
+        self._reset_completer()
 
-    def clear(self):
-        self.tit.clear()
-        self.num.clear()
-        self.pub.clear()
-        self.rls.clear()
-        self.tags.clear()
+    def _reset_completer(self):
+        tags = [t.tag for t in functions.get_all_tags(self._session)]
+        self._tags.set_completer(tags)
 
     def set_path(self, path: str):
-        self.path = path
-        if pdf := functions.get_pdf_by_path(self.session, self.path):
+        self._path = path
+        if pdf := functions.get_pdf_by_path(self._session, self._path):
             info = {k: getattr(pdf, k) for k in ("tit", "num", "pub", "rls", "tags")}
-            self.set_info(info)
+            self._set_info(info)
         else:
             self.clear()
 
-    def refresh_completer(self):
-        tags = [t.tag for t in functions.get_all_tags(self.session)]
-        self.tags.set_completer(tags)
+    def clear(self):
+        self._tit.clear()
+        self._num.clear()
+        self._pub.clear()
+        self._rls.clear()
+        self._tags.clear()
