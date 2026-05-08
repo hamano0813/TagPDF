29e2e6b3c02196e5e8b9927093c066df63abcd4c
Hamano0813 <hamano0813@gmail.com>
2026-05-08 21:39:14 +0800

    优化主程序初始化逻辑，设置日志过滤规则和应用样式

--- src/main.py
+++ src/main.py
@@ -1,13 +1,16 @@
 import sys
 
-from PySide6 import QtWidgets, QtGui
+from PySide6 import QtCore, QtWidgets, QtGui
 from PySide6.QtCore import QFile, QIODeviceBase
 
 from ui.main_window import MainWindow
 from res import *
 
 if __name__ == "__main__":
+    QtCore.QLoggingCategory.setFilterRules("qt.pdf.links.warning=false")
     app = QtWidgets.QApplication(sys.argv)
+    app.styleHints().setColorScheme(QtCore.Qt.ColorScheme.Light)  # type: ignore[attr-defined]
+    app.setStyle("Fusion")
     qss = QFile(":/style.qss")
     qss.open(QIODeviceBase.OpenModeFlag.ReadOnly)
     stylesheet = qss.readAll().toStdString()
