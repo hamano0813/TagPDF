553555f1a587c5372e7520ec472211626af97258
Hamano0813 <hamano0813@gmail.com>
2026-05-08 20:10:20 +0800

    修复样式表加载逻辑，确保正确读取QSS文件

--- src/main.py
+++ src/main.py
@@ -1,15 +1,16 @@
 import sys
 
 from PySide6 import QtWidgets, QtGui
+from PySide6.QtCore import QFile, QIODeviceBase
 
 from ui.main_window import MainWindow
 from res import *
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
-    qss = QtCore.QFile(":/style.qss")
-    qss.open(QtCore.QFile.ReadOnly)
-    stylesheet = qss.readAll().data().decode()
+    qss = QFile(":/style.qss")
+    qss.open(QIODeviceBase.OpenModeFlag.ReadOnly)
+    stylesheet = qss.readAll().toStdString()
     app.setStyleSheet(stylesheet)
     window = MainWindow()
     window.setWindowIcon(QtGui.QIcon(":/icon.png"))
